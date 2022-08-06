23e4d5196cf7b84ca4ab29ba6c1a6a6c4a9a18b5
Raul <raulbv.personal@gmail.com>
2022-08-06 20:47:30 +1000

    build: added worflow for gh action deploy

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220806.202949';
+const cacheName = 'chirpy-20220806.204420';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
