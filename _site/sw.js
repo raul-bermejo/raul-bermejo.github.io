69076dbb8ba9599647e2843e1c3e13dadf68d67a
Raul <raulbv.personal@gmail.com>
2022-08-06 21:47:23 +1000

    deploy

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220806.212144';
+const cacheName = 'chirpy-20220806.214707';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
