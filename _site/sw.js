4e489cd7e4c3d8569bbd7a33351aa25342b3a377
Raul <raulbv.personal@gmail.com>
2022-08-15 21:52:52 +1000

    redeploy

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220807.112026';
+const cacheName = 'chirpy-20220815.215213';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
