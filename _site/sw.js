7260e41725db6134dcedbb1dceebe4d80e49612a
Raul <raulbv.personal@gmail.com>
2023-10-12 16:35:35 +1100

    feat: formatted fml pt.I

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231012.152708';
+const cacheName = 'chirpy-20231012.153440';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
